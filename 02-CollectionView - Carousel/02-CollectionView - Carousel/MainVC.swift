04f622ee14cf73843ea755ab49b741fdf22a7878
ugurhmz <craxx67@gmail.com>
2022-03-16 22:33:09 +0300

    CollectionView int

---
+++ 02-CollectionView - Carousel/02-CollectionView - Carousel/MainVC.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  02-CollectionView - Carousel
+//
+//  Created by ugur-pc on 16.03.2022.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+    }
+
+
+}
+
